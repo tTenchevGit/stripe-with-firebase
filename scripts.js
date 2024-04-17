3538de64bba44ca4be7d58324fd1f4f3fdfb25db
Toni Tenchev <tonitenchev93@gmail.com>
2024-04-17 09:05:59 +0100

    no api

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
   
  const firebaseConfig = {
-  apiKey: "AIzaSyBYmVOHLOIPLDHlyAVA9-BclWLuHWxh8xk",
-  authDomain: "new-stripe.firebaseapp.com",
-  projectId: "new-stripe",
-  storageBucket: "new-stripe.appspot.com",
-  messagingSenderId: "648908661156",
-  appId: "1:648908661156:web:472801b5b5622770f7a131",
-  measurementId: "G-C90VPF06DT"
+  
 };
 
   // Initialize Firebase
@@ -14,7 +8,7 @@
 
   const checkoutButton = document.getElementById('checkout-button')
   const createStripeCheckout = firebase.functions().httpsCallable('createStripeCheckout')
-  const stripe = Stripe('pk_live_51NdcTXBeHRRqjgdXrvLHLfDg0Mu5qkyO9PBBMAMVGMtqGT4hDoEsDiDPKRnQS1g5YJTzfdJ6Jz36F5gfUbmbo45R00NfnUoeGA')
+  const stripe = Stripe('')
   
   checkoutButton.addEventListener('click', () => {
     createStripeCheckout()
